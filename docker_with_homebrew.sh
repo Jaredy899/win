693c9fdb132da9d87f97e69e483d35afe334f1b9
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-07-21 11:47:41 -0400

    Update docker_with_homebrew.sh

--- docker_with_homebrew.sh
+++ docker_with_homebrew.sh
@@ -2,22 +2,19 @@
 
 set -o errexit
 set -o nounset
-IFS=$(printf '
-	')
+IFS=$(printf '\n\t')
+
+# Variables
+USER="jared"  # Replace with your username
 
 # Docker, Nala, timezone
 sudo apt install sudo -y
 sudo apt install nala -y
 sudo timedatectl set-timezone America/New_York
 curl -sSL https://get.docker.com | sudo sh
-printf '
-Docker installed successfully
+printf '\nDocker installed successfully\n\n'
 
-'
-
-printf 'Waiting for Docker to start...
-
-'
+printf 'Waiting for Docker to start...\n\n'
 while ! sudo systemctl is-active --quiet docker; do
     sleep 1
 done
@@ -31,30 +28,33 @@ chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 sudo docker compose version
 
 # Portainer
-sudo docker volume create portainer_data
-sudo docker run -d   -p 8000:8000   -p 9000:9000   --name=portainer   --restart=always   -v /var/run/docker.sock:/var/run/docker.sock   -v portainer_data:/data   portainer/portainer-ce:latest
+docker volume create portainer_data
+docker run -d \
+  -p 8000:8000 \
+  -p 9000:9000 \
+  --name=portainer \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v portainer_data:/data \
+  portainer/portainer-ce:latest
 
-printf 'Waiting for Portainer to start...
-
-'
-until [ "$(sudo docker inspect -f '{{.State.Status}}' portainer)" = "running"; do
+printf 'Waiting for Portainer to start...\n\n'
+until [ "$(docker inspect -f '{{.State.Status}}' portainer)" = "running" ]; do
     sleep 1
 done
 
-printf '
-Portainer started successfully
-
-'
+printf '\nPortainer started successfully\n\n'
 
 # Homebrew
-yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh -o /tmp/install_homebrew.sh"
+sudo -u $USER bash -c "yes '' | bash /tmp/install_homebrew.sh
 
 # Configure Homebrew
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/jared/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo -u $USER bash -c '(echo; echo "eval \$($(/home/linuxbrew/.linuxbrew/bin/brew shellenv))") >> /home/$USER/.bashrc'
+sudo -u $USER bash -c 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
 
 # Install build-essential
 sudo apt-get install build-essential -y
 
 # Install gcc using Homebrew
-brew install gcc
+sudo -u $USER bash -c "brew install gcc"
