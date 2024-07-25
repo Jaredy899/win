d836524d59dad18a8dd3e90ade94ab1e551c65f0
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-07-25 14:06:18 -0400

    Update docker_with_homebrew.sh

--- docker_with_homebrew.sh
+++ docker_with_homebrew.sh
@@ -28,8 +28,8 @@ chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 sudo docker compose version
 
 # Portainer
-docker volume create portainer_data
-docker run -d \
+sudo docker volume create portainer_data
+sudo docker run -d \
   -p 8000:8000 \
   -p 9000:9000 \
   --name=portainer \
