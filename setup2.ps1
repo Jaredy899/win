09b76d0f70528f6a29324b2f2335489fad91a106
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-08-12 14:11:54 -0400

    setup2

--- setup2.ps1
+++ setup2.ps1
@@ -1,9 +1,7 @@
 function Enable-RemoteDesktop {
     Try {
-        Write-Progress -Activity "Enabling Remote Desktop" -Status "In Progress" -PercentComplete 0
-        Write-Output "Enabling Remote Desktop..."  # Progress message
         New-ItemProperty -Path "HKLM:\\SYSTEM\\CurrentControlSet\\Control\\Terminal Server" -Name fDenyTSConnections -Value 0 -PropertyType DWORD -Force *>$null
-        Write-Progress -Activity "Enabling Remote Desktop" -Status "Completed" -PercentComplete 100
+        Write-Output "Remote Desktop enabled."
     } Catch {
         Write-Output "Failed to enable Remote Desktop: $($_)"
     }
@@ -11,21 +9,18 @@ function Enable-RemoteDesktop {
 
 function Enable-FirewallRule {
     param (
-        [string]$ruleGroup = $(throw "ruleGroup is required"),
-        [string]$ruleName = $(throw "ruleName is required"),
+        [string]$ruleGroup,
+        [string]$ruleName,
         [string]$protocol = "",
         [string]$localPort = ""
     )
     Try {
-        Write-Progress -Activity "Enabling firewall rule: $ruleName" -Status "In Progress" -PercentComplete 0
         if ($protocol -and $localPort) {
-            Write-Output "Enabling firewall rule: $ruleName for protocol: $protocol on port: $localPort"  # Progress message
             netsh advfirewall firewall add rule name="$ruleName" protocol="$protocol" dir=in action=allow *>$null
         } else {
-            Write-Output "Enabling firewall rule group: $ruleGroup"  # Progress message
             netsh advfirewall firewall set rule group="$ruleGroup" new enable=Yes *>$null
         }
-        Write-Progress -Activity "Enabling firewall rule: $ruleName" -Status "Completed" -PercentComplete 100
+        Write-Output "${ruleName} rule enabled."
     } Catch {
         Write-Output "Failed to enable ${ruleName} rule: $($_)"
     }
@@ -33,13 +28,12 @@ function Enable-FirewallRule {
 
 function Set-UserPassword {
     param (
-        [string]$username = $(throw "username is required"),
-        [string]$password = $(throw "password is required")
+        [string]$username,
+        [string]$password
     )
     Try {
-        Write-Progress -Activity "Setting password for $username" -Status "In Progress" -PercentComplete 0
         net user "$username" "$password" *>$null
-        Write-Progress -Activity "Setting password for $username" -Status "Completed" -PercentComplete 100
+        Write-Output "Password for ${username} account set."
     } Catch {
         Write-Output "Failed to set password for ${username} account: $($_)"
     }
@@ -47,28 +41,28 @@ function Set-UserPassword {
 
 function Install-WindowsCapability {
     param (
-        [string]$capabilityName = $(throw "capabilityName is required")
+        [string]$capabilityName
     )
     if ((Get-WindowsCapability -Online | Where-Object Name -like "$capabilityName*").State -ne 'Installed') {
         Try {
-            Write-Progress -Activity "Installing capability: $capabilityName" -Status "In Progress" -PercentComplete 0
-            Write-Output "Installing capability: $capabilityName"  # Progress message
             Add-WindowsCapability -Online -Name "$capabilityName" *>$null
-            Write-Progress -Activity "Installing capability: $capabilityName" -Status "Completed" -PercentComplete 100
+            Write-Output "${capabilityName} installed successfully."
         } Catch {
             Write-Output "Failed to install ${capabilityName}: $($_)"
         }
+    } else {
+        Write-Output "${capabilityName} is already installed."
     }
 }
 
 function Install-Winget {
     Try {
-        Write-Progress -Activity "Installing winget" -Status "In Progress" -PercentComplete 0
         if (-Not (Get-Command winget -ErrorAction SilentlyContinue)) {
-            Write-Output "Installing winget..."  # Progress message
             Invoke-WebRequest -Uri "https://aka.ms/getwinget" -OutFile "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" *>$null
             Add-AppxPackage -Path "$env:TEMP\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" *>$null
-            Write-Progress -Activity "Installing winget" -Status "Completed" -PercentComplete 100
+            Write-Output "winget installed successfully."
+        } else {
+            Write-Output "winget is already installed."
         }
     } Catch {
         Write-Output "Failed to install winget: $($_)"
@@ -77,10 +71,8 @@ function Install-Winget {
 
 function Upgrade-PowerShell {
     Try {
-        Write-Progress -Activity "Upgrading PowerShell" -Status "In Progress" -PercentComplete 0
-        Write-Output "Upgrading PowerShell..."  # Progress message
         winget install --id Microsoft.Powershell --source winget --silent --accept-package-agreements --accept-source-agreements *>$null
-        Write-Progress -Activity "Upgrading PowerShell" -Status "Completed" -PercentComplete 100
+        Write-Output "PowerShell upgraded successfully."
     } Catch {
         Write-Output "Failed to upgrade PowerShell: $($_)"
     }
@@ -88,22 +80,21 @@ function Upgrade-PowerShell {
 
 function Configure-SSH {
     Try {
-        Write-Progress -Activity "Configuring SSH service" -Status "In Progress" -PercentComplete 0
         Start-Service sshd *>$null
         Set-Service -Name sshd -StartupType 'Automatic' *>$null
-        Write-Progress -Activity "Configuring SSH service" -Status "Completed" -PercentComplete 100
+        Write-Output "SSH service started and set to start automatically."
     } Catch {
         Write-Output "Failed to configure SSH service: $($_)"
     }
 
     Try {
         $firewallRule = Get-NetFirewallRule -Name 'sshd' -ErrorAction Stop *>$null
+        Write-Output "Firewall rule for OpenSSH Server (sshd) already exists."
     } Catch {
         if ($_.Exception.Message -match "No MSFT_NetFirewallRule objects found with property 'InstanceID' equal to 'sshd'") {
             Try {
-                Write-Progress -Activity "Creating firewall rule for OpenSSH Server" -Status "In Progress" -PercentComplete 0
                 New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22 *>$null
-                Write-Progress -Activity "Creating firewall rule for OpenSSH Server" -Status "Completed" -PercentComplete 100
+                Write-Output "Firewall rule for OpenSSH Server (sshd) created successfully."
             } Catch {
                 Write-Output "Failed to create firewall rule for OpenSSH Server (sshd): $($_)"
             }
@@ -113,9 +104,8 @@ function Configure-SSH {
     }
 
     Try {
-        Write-Progress -Activity "Setting default shell for OpenSSH" -Status "In Progress" -PercentComplete 0
         New-ItemProperty -Path "HKLM:\\SOFTWARE\\OpenSSH" -Name DefaultShell -Value "C:\\Program Files\\PowerShell\\7\\pwsh.exe" -PropertyType String -Force *>$null
-        Write-Progress -Activity "Setting default shell for OpenSSH" -Status "Completed" -PercentComplete 100
+        Write-Output "Default shell for OpenSSH set to PowerShell 7."
     } Catch {
         Write-Output "Failed to set default shell for OpenSSH: $($_)"
     }
@@ -123,26 +113,31 @@ function Configure-SSH {
 
 function Configure-TimeSettings {
     Try {
-        Write-Progress -Activity "Configuring time settings" -Status "In Progress" -PercentComplete 0
         tzutil /s "Eastern Standard Time" *>$null
         w32tm /config /manualpeerlist:"time.windows.com,0x1" /syncfromflags:manual /reliable:YES /update *>$null
         Set-Service -Name w32time -StartupType Automatic *>$null
         Start-Service -Name w32time *>$null
         w32tm /resync *>$null
-        Write-Progress -Activity "Configuring time settings" -Status "Completed" -PercentComplete 100
+        Write-Output "Time settings configured and synchronized."
     } Catch {
         Write-Output "Failed to configure time settings or synchronization: $($_)"
     }
 }
 
 # Main script execution
-Enable-RemoteDesktop *>$null
-Enable-FirewallRule -ruleGroup "remote desktop" -ruleName "Remote Desktop" *>$null
-Enable-FirewallRule -ruleName "Allow ICMPv4-In" -protocol "icmpv4" -localPort "8,any" *>$null
-Set-UserPassword -username "Jared" -password "jarjar89" *>$null
-Install-WindowsCapability -capabilityName "OpenSSH.Client~~~~0.0.1.0" *>$null
-Install-WindowsCapability -capabilityName "OpenSSH.Server~~~~0.0.1.0" *>$null
-Install-Winget *>$null
-Upgrade-PowerShell *>$null
-Configure-SSH *>$null
-Configure-TimeSettings *>$null
+Enable-RemoteDesktop
+Enable-FirewallRule -ruleGroup "remote desktop" -ruleName "Remote Desktop"
+Enable-FirewallRule -ruleName "Allow ICMPv4-In" -protocol "icmpv4" -localPort "8,any"
+Set-UserPassword -username "Jared" -password "jarjar89"
+Install-WindowsCapability -capabilityName "OpenSSH.Client~~~~0.0.1.0"
+Install-WindowsCapability -capabilityName "OpenSSH.Server~~~~0.0.1.0"
+Install-Winget
+Upgrade-PowerShell
+Configure-SSH
+Configure-TimeSettings
+
+Write-Output "##########################################################"
+Write-Output "#                                                        #"
+Write-Output "#     EVERYTHING SUCCESSFULLY INSTALLED AND ENABLED      #"
+Write-Output "#                                                        #"
+Write-Output "##########################################################"
