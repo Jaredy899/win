3b86b68321151057fa1825428910fe23f3d1b16d
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-08-25 16:48:02 -0400

    apps

--- my_powershell/apps_install.ps1
+++ my_powershell/apps_install.ps1
@@ -14,9 +14,23 @@ $apps = @(
 function Install-Apps {
     foreach ($app in $apps) {
         Write-Host "Installing $app using Winget..."
-        winget install --id $app -e
-        if ($LASTEXITCODE -ne 0) {
-            Write-Error "Failed to install $app. Please check your internet connection or the app name."
+        
+        try {
+            # Run the Winget install command and capture the output
+            $installResult = winget install --id $app --accept-package-agreements --accept-source-agreements -e 2>&1
+            
+            # Check if the app is already installed and up-to-date
+            if ($installResult -match "No available upgrade found" -or $installResult -match "already installed" -or $installResult -match "Up to date") {
+                Write-Host "$app is already installed and up to date."
+            }
+            elseif ($LASTEXITCODE -eq 0) {
+                Write-Host "$app installed successfully."
+            } else {
+                Write-Error "Failed to install $app. Please check your internet connection or the app name."
+            }
+        }
+        catch {
+            Write-Error "An error occurred while installing $app. Error: $_"
         }
     }
 }
