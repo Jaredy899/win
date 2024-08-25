9f151601ee8d428d93f187a8d12fe2b98fe80b1d
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-08-25 12:23:14 -0400

    changes

--- install_winget/Install_winget.ps1
+++ install_winget/Install_winget.ps1
@@ -1,8 +1,20 @@
-# Check if Winget is installed by executing the Test_winget.ps1 script from GitHub
-$isWingetInstalledScript = "https://raw.githubusercontent.com/Jaredy899/setup/main/Test_winget.ps1"
-$isWingetInstalled = Invoke-Expression (Invoke-WebRequest -Uri $isWingetInstalledScript -UseBasicParsing).Content
+# Define base URL for the required scripts
+$baseUrl = "https://raw.githubusercontent.com/Jaredy899/win/main/install_winget/"
+
+# Define URLs for each script
+$isWingetInstalledScriptUrl = "${baseUrl}Test_winget.ps1"
+$wingetPrereqScriptUrl = "${baseUrl}Winget_prereq.ps1"
+$wingetLatestScriptUrl = "${baseUrl}Winget_latest.ps1"
 
 try {
+    # Check if Winget is installed by executing the Test_winget.ps1 script from GitHub
+    if ((Invoke-WebRequest -Uri $isWingetInstalledScriptUrl -UseBasicParsing -ErrorAction Stop).StatusCode -eq 200) {
+        $isWingetInstalled = Invoke-Expression (Invoke-WebRequest -Uri $isWingetInstalledScriptUrl -UseBasicParsing).Content
+    } else {
+        Write-Host "Failed to fetch the Test_winget.ps1 script." -ForegroundColor Red
+        return
+    }
+
     if ($isWingetInstalled -eq "installed") {
         Write-Host "`nWinget is already installed.`r" -ForegroundColor Green
         return
@@ -21,19 +33,26 @@ try {
         return
     }
 
-    # Install Winget via GitHub method.
     Write-Host "Downloading Winget Prerequisites`n"
-    
+
     # Execute Winget_prereq.ps1 script from GitHub
-    $wingetPrereqScript = "https://raw.githubusercontent.com/Jaredy899/setup/main/Winget_prereq.ps1"
-    Invoke-Expression (Invoke-WebRequest -Uri $wingetPrereqScript -UseBasicParsing).Content
-    
+    if ((Invoke-WebRequest -Uri $wingetPrereqScriptUrl -UseBasicParsing -ErrorAction Stop).StatusCode -eq 200) {
+        Invoke-Expression (Invoke-WebRequest -Uri $wingetPrereqScriptUrl -UseBasicParsing).Content
+    } else {
+        Write-Host "Failed to fetch the Winget_prereq.ps1 script." -ForegroundColor Red
+        return
+    }
+
     Write-Host "Downloading Winget and License File`r"
-    
+
     # Execute Winget_latest.ps1 script from GitHub
-    $wingetLatestScript = "https://raw.githubusercontent.com/Jaredy899/setup/main/Winget_latest.ps1"
-    Invoke-Expression (Invoke-WebRequest -Uri $wingetLatestScript -UseBasicParsing).Content
-    
+    if ((Invoke-WebRequest -Uri $wingetLatestScriptUrl -UseBasicParsing -ErrorAction Stop).StatusCode -eq 200) {
+        Invoke-Expression (Invoke-WebRequest -Uri $wingetLatestScriptUrl -UseBasicParsing).Content
+    } else {
+        Write-Host "Failed to fetch the Winget_latest.ps1 script." -ForegroundColor Red
+        return
+    }
+
     Write-Host "Installing Winget with Prerequisites`r"
     Add-AppxProvisionedPackage -Online -PackagePath $ENV:TEMP\Microsoft.DesktopAppInstaller.msixbundle -DependencyPackagePath $ENV:TEMP\Microsoft.VCLibs.x64.Desktop.appx, $ENV:TEMP\Microsoft.UI.Xaml.x64.appx -LicensePath $ENV:TEMP\License1.xml
     Write-Host "Manually adding Winget Sources, from Winget CDN."
@@ -42,7 +61,6 @@ try {
     Write-Host "Enabling NuGet and Module..."
     Install-PackageProvider -Name NuGet -Force
     Install-Module -Name Microsoft.WinGet.Client -Force
-    # Winget only needs a refresh of the environment variables to be used.
     Write-Output "Refreshing Environment Variables...`n"
     $ENV:PATH = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 } catch {
