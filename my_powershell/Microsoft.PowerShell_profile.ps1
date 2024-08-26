e105e3712f70f88a32e9720617b1810591ffbfe6
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-08-26 13:11:59 -0400

    Update Microsoft.PowerShell_profile.ps1

--- my_powershell/Microsoft.PowerShell_profile.ps1
+++ my_powershell/Microsoft.PowerShell_profile.ps1
@@ -17,6 +17,12 @@ if ($Host.Name -eq 'ConsoleHost' -or $Host.Name -eq 'Windows Terminal') {
         if (Get-Command starship -ErrorAction SilentlyContinue) {
             Invoke-Expression (& { starship init powershell })
         }
+        
+        # Ensure Terminal-Icons module is installed before importing
+        if (-not (Get-Module -ListAvailable -Name Terminal-Icons)) {
+            Install-Module -Name Terminal-Icons -Scope CurrentUser -Force -SkipPublisherCheck
+        }
+        Import-Module -Name Terminal-Icons
 
         # Define aliases and functions
         function ff {
