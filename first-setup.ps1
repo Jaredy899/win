9275da9f179ac2ee8c1485563fad9e0f0723a21b
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2024-10-12 00:52:24 -0400

    fix win github path

--- first-setup.ps1
+++ first-setup.ps1
@@ -3,7 +3,7 @@ $GITPATH = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Write-Host "GITPATH is set to: $GITPATH"
 
 # GitHub URL base for the necessary configuration files
-$GITHUB_BASE_URL = "https://raw.githubusercontent.com/Jaredy899/setup/main"
+$GITHUB_BASE_URL = "https://raw.githubusercontent.com/Jaredy899/setup/main/win"
 
 # Function to check if the script is running with administrator privileges
 function Test-AdminRights {
